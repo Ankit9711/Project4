aee139eef4ec4ab6b1b2538547924b0d920a4e32
Ankit Kumar <sd420479@gmail.com>
2026-01-11 01:59:36 +0530

    Refactor expense input and categorization logic
    Refactor expense tracking logic to improve categorization and output formatting.

--- PROJECT4.py
+++ PROJECT4.py
@@ -5,29 +5,27 @@ status=""
 for i in range(1,exp+1) :
     Exp_am=int(input(f"Expenses{i} Amount  : "))
     if(Exp_am>500) : status="High Expense Detected"
-    Exp_categ=(input(f"Expenses{i} Category: "))
-    print()
-    L_Exp.append(Exp_am)
-    d_CategwithExp.get(Exp_categ)
-    d_CategwithExp[Exp_categ]=Exp_am
+    Exp_categ=input(f"Expenses{i} Category: ").upper()
+    print("\n\n")
+    d_CategwithExp[Exp_categ]=d_CategwithExp.get(Exp_categ,0)+Exp_am
 print(f"{status}\n")
+for i in d_CategwithExp.values() :
+    L_Exp.append(i)
 Total=0
 for item  in L_Exp :
     Total+=item
 print(f"Total Expense  ={Total}")
 Avgexp=Total/exp
 print(f"Average Expense={round(Avgexp)}")
-print(f"\n")
+print("\n\n")
 print("Category_Wise Expenses:")
-min=0
 max=[]
+L_Exp.sort()
+min=L_Exp[-1]
 for i,j in d_CategwithExp.items() :
     print(f"{i} : {j}")
-    if(min<=j) : 
-        min=j 
-for i,j in d_CategwithExp.items() : 
-    if(min==j) : max.append(i)
-print(f"\n")
+    if(j==min)  :  max.append(i) 
+print("\n\n")
 print(f"Highest Spending Category:{max}")
 print(f"Spending Status: ",end="")
 if(Avgexp<=200)   : print("Controlled Spending")
